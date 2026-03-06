4331cb0540100233c883f0ec2284a3efb04190a1
oluwatobi <luponet07@gmail.com>
2026-03-06 02:11:58 +0100

    fixed go valodation issue

--- internal/auth/types.go
+++ internal/auth/types.go
@@ -11,5 +11,5 @@ type CreateUserRequest struct {
 
 type LoginRequest struct {
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required, min=6"`
+	Password string `json:"password" binding:"required,min=6"`
 }
